e35821100e1715b09a3e1ffc387e4487d14e4614
Krishika R P <282138647+krishikaRP@users.noreply.github.com>
2026-09-25 22:07:10 +0530

    Add functionality to calculate working hours

---
+++ Working hours.java
@@ -0,0 +1,11 @@
+Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0)
+        {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            System.out.println(Y - X);
+        }
