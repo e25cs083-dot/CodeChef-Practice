37cff427e20ebed25921fdcbf9ac93d17c5f56d7
Krishika R P <282138647+krishikaRP@users.noreply.github.com>
2026-09-25 22:05:45 +0530

    Create donations.java

---
+++ donations.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0)
+        {
+            int X = sc.nextInt();
+            int N = sc.nextInt();
+
+            System.out.println(X - N);
+        }
+    }
+}
