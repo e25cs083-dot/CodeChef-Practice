5ce701357ffb3d785fd6064defcff718ff2acd02
Krishika R P <282138647+krishikaRP@users.noreply.github.com>
2026-09-26 21:52:50 +0530

    Add input check for values between 6 and 8

---
+++ Puzzle Hunt.java
@@ -0,0 +1,11 @@
+Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n >= 6 && n <= 8) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        sc.close();
